b952595e50a35bcae1ec24cdf59504103510f4b1
Tanapoom Sermchaiwong <tanapoom.se@gmail.com>
2017-08-12 22:27:58 +0700

    Fully Functioning - YAY
    Matches a block of the "outline" picture with a corresponding picture with similar luminance values.

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,20 @@ import java.util.ArrayList;
  * Created by Poom on 8/12/2017.
  */
 public class Main {
+    static BufferedImage finalImage;
+    static String args[];
+
+    static int blocksize = 30;
+
+    static JTextField outlineLocation;
+    static JPanel panel;
+
+    static PhotoProcessor.ResourceBank resourceBank;
+
     public static void main(String args[])
     {
+        Main.args = args;
+
         String outline  = args[0];
         String resource = args[1];
 
@@ -38,7 +50,7 @@ public class Main {
                 try
                 {
                     BufferedImage toLoad = ImageIO.read(file);
-                    BufferedImage compressed = new BufferedImage(PhotoProcessor.blocksize, PhotoProcessor.blocksize, BufferedImage.TYPE_INT_ARGB);
+                    BufferedImage compressed = new BufferedImage(blocksize, blocksize, BufferedImage.TYPE_INT_ARGB);
                     compressed.createGraphics();
                     compressed.getGraphics().drawImage(toLoad,0,0,compressed.getWidth(), compressed.getHeight(),null);
                     resourceList.add(compressed);
@@ -50,9 +62,14 @@ public class Main {
             }
         }
 
-        PhotoProcessor.ResourceBank resourceBank = PhotoProcessor.generatePhotoResourceBank(resourceList);
+        resourceBank = PhotoProcessor.generatePhotoResourceBank(resourceList);
 
-        File outlineFile = new File(outline);
+        genUI();
+    }
+
+    public static void createMosaic()
+    {
+        File outlineFile = new File(outlineLocation.getText());
         BufferedImage outlineImage = null;
         try
         {
@@ -63,38 +80,69 @@ public class Main {
             e.printStackTrace();
         }
 
-        BufferedImage finalImage = PhotoProcessor.generateToOutline(outlineImage, resourceBank);
+        finalImage = PhotoProcessor.generateToOutline(outlineImage, resourceBank, blocksize);
 
+        panel.repaint();
+    }
+
+    public static void genUI()
+    {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1900,1080);
+        frame.setSize(1800,900);
         frame.setLayout(new FlowLayout());
 
-        JPanel panel = new JPanel(){
+        panel = new JPanel(){
             protected void paintComponent(Graphics g)
             {
                 super.paintComponent(g);
-                g.drawImage(finalImage,0,0,finalImage.getWidth(), finalImage.getHeight(),null);
+                if(finalImage != null) {
+                    double scale = 1;
+                    if (finalImage.getWidth() > 880.0) {
+                        scale = 880.0 / finalImage.getWidth();
+                    } else if (finalImage.getHeight() > 1780) {
+                        scale = 1780.0 / finalImage.getHeight();
+                    }
+                    g.drawImage(finalImage, 0, 0, (int) (finalImage.getWidth() * scale), (int) (finalImage.getHeight() * scale), null);
+                }
             }
         };
 
 
-        panel.setPreferredSize(new Dimension(1880,1060));
+        panel.setPreferredSize(new Dimension(1780,880));
         panel.setBackground(Color.BLACK);
 
         JButton button = new JButton("Save");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    File outputfile = new File(args[2]);
-                    ImageIO.write(finalImage, "png", outputfile);
+                if(finalImage != null) {
+                    try {
+                        File outputfile = new File(args[2]);
+                        ImageIO.write(finalImage, "png", outputfile);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
                 }
-                catch(IOException ex)
-                {ex.printStackTrace();}
             }
         });
 
+        JLabel jLable = new JLabel("Location of Outline");
+
+        outlineLocation = new JTextField(args[0]);
+
+        JButton create = new JButton("Create");
+        create.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createMosaic();
+            }
+        });
+
+
+        frame.add(jLable);
+        frame.add(outlineLocation);
+        frame.add(create);
         frame.add(button);
         frame.add(panel);
         frame.setVisible(true);
--- src/PhotoProcessor.java
+++ src/PhotoProcessor.java
@@ -11,9 +11,6 @@ import java.util.List;
 
 public class PhotoProcessor {
 
-    public static int blocksize = 20;
-    static double compressionConstant = 1.3;
-
     public static ResourceBank generatePhotoResourceBank(ArrayList<BufferedImage> resourceList) {
         ResourceBank resourceBank = new ResourceBank();
 
@@ -58,48 +55,20 @@ public class PhotoProcessor {
             resourceBank.luminanceMap.put(averageLuminance, image);
         }
 
-        resourceBank.generateDarknessThreshold();
-
         return resourceBank;
     }
 
-    public static BufferedImage generateToOutline(BufferedImage outline, ResourceBank resourceBank) {
+    public static BufferedImage generateToOutline(BufferedImage outline, ResourceBank resourceBank, int blocksize) {
         BufferedImage finalImage = new BufferedImage(outline.getWidth(), outline.getHeight(), BufferedImage.TYPE_INT_ARGB);
         finalImage.createGraphics();
         Graphics2D g = (Graphics2D)finalImage.getGraphics();
-
-        Set<Float> keyset = resourceBank.luminanceMap.keySet();
-
-        int firstBrightIndex = 0;
-
-        int count = 0;
-        for(float luminance: keyset)
-        {
-            if(luminance > resourceBank.darknessThreshold)
-            {
-                firstBrightIndex = count;
-                break;
-            }
-            ++count;
-        }
-
-        ArrayList<Float> keyList = new ArrayList<>();
-
-        keyList.addAll(keyset);
-
-        ArrayList<Float> brightList = new ArrayList<>();
-        brightList.addAll(keyList.subList(firstBrightIndex, keyList.size()));
-
-        ArrayList<Float> darkList = new ArrayList<>();
-        darkList.addAll(keyList.subList(0, firstBrightIndex));
-
-        Random random = new Random();
 
         for(int x = 0; x < finalImage.getWidth() - blocksize + 1; x+=blocksize)
         {
             for(int y = 0; y < finalImage.getHeight() - blocksize + 1; y+=blocksize)
             {
-                boolean hasBlack = false;
+                float luminanceSum = 0;
+
                 for(int innerx = x; innerx < x + blocksize; innerx++)
                 {
                     for(int innery = y; innery < y + blocksize; innery++)
@@ -114,50 +83,37 @@ public class PhotoProcessor {
                         //calculate luminance in range from 0.0 to 1.0 using srgb luminance constants
                         float luminance = (red * 0.2126f + green * 0.7152f + blue * 0.0722f) / 255;
 
-                        hasBlack = hasBlack || (luminance < 0.5);
+                       luminanceSum += luminance;
                     }
                 }
 
-                float luminance;
-                if(!hasBlack)
-                {
-                    if(brightList.isEmpty())
-                    {
-                        brightList.addAll(keyList.subList(firstBrightIndex, keyList.size()));
-                    }
-                    luminance = brightList.remove(random.nextInt(brightList.size()));
-                }
-                else
-                {
-                    if(darkList.isEmpty())
-                    {
-                        darkList.addAll(keyList.subList(0, firstBrightIndex));
-                    }
-                    luminance = darkList.remove(random.nextInt(darkList.size()));
-                }
+                float avgLumin = luminanceSum / (blocksize * blocksize);
 
-                g.drawImage(resourceBank.luminanceMap.get(luminance), x , y , blocksize, blocksize, null);
+                BufferedImage chosenImage = getClosestLuminanceMatch(avgLumin, resourceBank);
+
+                g.drawImage(chosenImage, x , y , blocksize, blocksize, null);
             }
         }
 
         return finalImage;
     }
 
+    public static BufferedImage getClosestLuminanceMatch(float outlineLumin, ResourceBank resourceBank)
+    {
+
+        float closestLumin = -999;
+
+        for(float resLumin: resourceBank.luminanceMap.keySet())
+        {
+            if(Math.abs(resLumin - outlineLumin) < Math.abs(closestLumin - outlineLumin))
+            {
+                closestLumin = resLumin;
+            }
+        }
+        return resourceBank.luminanceMap.get(closestLumin);
+    }
+
     public static class ResourceBank {
         TreeMap<Float, BufferedImage> luminanceMap = new TreeMap<>();
-        float darknessThreshold;
-
-        public void generateDarknessThreshold()
-        {
-            float sum = 0;
-            int count = 0;
-            for(float luminance: luminanceMap.keySet())
-            {
-                sum += Math.pow((luminance*10),1/(compressionConstant));
-                ++count;
-            }
-
-            darknessThreshold = sum/10 / count;
-        }
     }
 }
